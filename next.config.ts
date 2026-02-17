05f869e7141aa1fd2d295026320ea6ce8b0fdccd
Yekna <urosjeknic@gmail.com>
2026-02-17 17:05:11 +0100

    configure next.config.ts to accept images from jobicy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{ hostname: "jobicy.com" }],
+  },
 };
 
 export default nextConfig;
